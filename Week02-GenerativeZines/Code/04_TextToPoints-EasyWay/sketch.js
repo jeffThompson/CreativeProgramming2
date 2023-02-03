28e03f3f666c023ad5052ad907db1d2dc7065b34
Jeff Thompson <mail@jeffreythompson.org>
2023-02-03 13:39:42 -0500

    Added second part and code examples

---
+++ Week02-GenerativeZines/Code/04_TextToPoints-EasyWay/sketch.js
@@ -0,0 +1,108 @@
+/*
+TEXT TO POINTS (the easy way)
+Jeff Thompson | 2021 | jeffreythompson.org
+
+What if we want to transform our text into different shapes?
+Or if we want the letters to have physics or other cool
+effects applied to them? There are a few ways we might approach
+this, but the easiest is to simply draw the text onscreen,
+then look at the pixels to see if a letter is present in a
+particular spot or not!
+
+Use the mouseX position to make the letters spread into
+a cloud and back again.
+
+SEE ALSO
++ ZXX, the font used here, is by Sang Mun – read more here!
+  https://walkerart.org/magazine/sang-mun-defiant-typeface-nsa-privacy
++ The next example (the 'hard way') shows how to get points
+  from the outline of a text!
+
+CHALLENGES
+1. Play with the parameters here and see how those changes
+   affect the overall look and feel of the sketch! Try dot 
+   size, the shape that's drawn, and the amount of distortion
+   as a starting point
+2. Can you make the shapes rotate as they distort too? Don't
+   use circles though, duh :)
+2. Since randomSeed(0) keeps the randomness exactly the same, 
+   when the letters become spread out the result is always 
+   the same. Can you come up with a way to change that seed
+   value when a certain condition is met?
+
+*/
+
+
+let gridSize = 10;  // spacing of dots (and their diameter)
+let points = [];    // list of points to draw dots at
+let font;
+
+
+function preload() {
+  font = loadFont('assets/ZXX-Bold.otf');
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  // to get the locations of the dots, we draw
+  // some text onscreen in setup() where it won't
+  // actually be seen
+  background(255);
+  textFont(font);
+  textSize(300);
+  textAlign(CENTER, CENTER);
+  fill(0);
+  noStroke();
+  text('LOL', width/2,height/2);
+  
+  // then, by reading the pixel value onscreen
+  // and, if it's black (we'll talk lots
+  // more about this in the next project), we
+  // capture the position in our list
+  loadPixels();
+  for (let y=0; y<height; y+=gridSize) {
+    for (let x=0; x<width; x+=gridSize) {
+      let r = get(x, y)[0];    // get the red value of the pixel
+      if (r < 127) {           // at x/y (will be 0–255)
+        
+        // vectors are super useful objects!
+        // they store x/y (or x/y/z) positions in
+        // a single variable – really helpful when
+        // you want to store a list of coordinates!
+        points.push(createVector(x,y));
+      }
+    }
+  }
+}
+
+
+function draw() {
+  background(220);
+  
+  // mouseX will change the amount of mutation in
+  // the letters (note: we use randomSeed(0) every frame
+  // so the letters don't just become a noisy cloud
+  // but behave in a consistent way – try removing the
+  // line and see what happens!)
+  let mutationAmt = map(mouseX, 0,width, 3,width/6);
+  randomSeed(0);
+  
+  // draw the points, with their mutations!
+  for (let i=0; i<points.length; i++) {
+    let x = points[i].x;
+    let y = points[i].y;
+    x += random(-mutationAmt,mutationAmt);
+    y += random(-mutationAmt,mutationAmt);
+
+    fill(0,50);     // shadow
+    noStroke();
+    circle(x+2,y+2, gridSize);
+    
+    fill(220);      // regular circle
+    stroke(0, 100);
+    circle(x,y, gridSize);
+  }
+}
+
---
+++ Week02-GenerativeZines/Part02-BookMockup.md
@@ -0,0 +1,41 @@
+# PART 2: BOOK MOCKUP
+
+### TLDR
+* Due Feb 14 ❤️  
+  * Bring your printed mockup to class
+  * Nothing to turn in on Canvas  
+* Add text to your project  
+* Output pages for your book  
+  * Laser print your pages **single-sided** in the Fab Lab  
+* Purchase additional materials as needed  
+
+- - -
+
+### CODE EXAMPLES/VIDEOS
+* [Code examples in the `p5.js` Editor](https://editor.p5js.org/jeffThompson/collections/XPqAUU6EI)  
+* [Video tutorials](https://youtube.com/playlist?list=PLsGCUnpinsDktRKA-93U6OXNqEKX8wuyN) – they're for a different assignment, but the technical stuff is still the same!  
+
+- - -
+
+### CREATE A MOCKUP  
+Last week, you starting building a system that can create varied output. Continuing this, your assignment this week is to further explore and refine your system's code, add text for at least some of the pages, and output a sequence of pages that can form your book.
+
+The approach here may vary quite a bit! You might consider...  
+
+* Save tons of versions, pick 8+ you like and arrange them manually  
+* Create an arc through the book where variable(s) change across the pages (hint: use a for-loop and generate the pages using a function)  
+* Iteratively output/refine the code, figuring out the sequence as you go  
+
+There are a few other details to think about...  
+
+
+* The number of pages in your book should be a multiple of four (one sheet of paper, folded in half, creates four pages)  
+  * Your book should have at least 8 pages but can have more!  
+* Aside from the first and last pages, everything else will be visible as two-page spreads, not singular images  
+* Full-bleed (the image going to the edge) is possible but your book will need to be trimmed after binding  
+* Cover paper? Binding style? Other materials?  
+
+Once you have your pages and their order sorted out, print them **single sided** in the Fab Lab. Be sure you have DuckBills in your account and note the Fab Lab hours. If you need additional paper or other materials, be sure to get those for next week as well.
+
+Nothing to turn in on Canvas, but bring your prints to class. Presentation and craft count towards your grade! No wrinkled or dirty prints please.
+
